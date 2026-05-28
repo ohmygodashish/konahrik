51ab2ef9c26bd11f5b36e171964c7a4d2b33faca
Ashish Prajapati <ashishanshprajapati@gmail.com>
2026-05-28 19:45:34 +0000

    feat: export open_position and close_position modules
    Register the new instruction modules in the instructions module index to make them accessible to the crate.

--- programs/konahrik/src/instructions.rs
+++ programs/konahrik/src/instructions.rs
@@ -1,5 +1,9 @@
+pub mod close_position;
 pub mod deposit_margin;
 pub mod initialize_amm;
+pub mod open_position;
 
+pub use close_position::*;
 pub use deposit_margin::*;
 pub use initialize_amm::*;
+pub use open_position::*;
